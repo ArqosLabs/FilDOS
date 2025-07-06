e73fccb2e055896c0694145e398171726dbc4e48
ahmedfahim21 <ahmed.fahim0207@gmail.com>
2025-07-06 20:58:02 +0530

    Deploy contract

---
+++ src/utils/contracts.ts
@@ -0,0 +1,18 @@
+import { ethers } from 'ethers';
+import ContractABI from '../../web3/ignition/deployments/chain-314159/artifacts/FILDOSModule#FolderNFT.json';
+import ContractAddress from '../../web3/ignition/deployments/chain-314159/deployed_addresses.json';
+
+const CONTRACT_ADDRESS = ContractAddress["FILDOSModule#FolderNFT"];
+
+type ContractSigner = 
+    | ethers.Provider 
+    | ethers.Signer 
+    | ethers.JsonRpcSigner;
+
+export const getContract = (signer: ContractSigner): ethers.Contract => {
+    return new ethers.Contract(
+        CONTRACT_ADDRESS,
+        ContractABI.abi,
+        signer
+    );
+};
